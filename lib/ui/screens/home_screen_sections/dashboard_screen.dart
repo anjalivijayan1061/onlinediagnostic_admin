61a354ea77ef9b16b8abc2428dfe10ccc9d6464d
Arjunraj Kokkadan <arjunrajkokkadan@gmail.com>
2023-04-18 10:25:19 +0530

    Admin complete

--- lib/ui/screens/home_screen_sections/dashboard_screen.dart
+++ lib/ui/screens/home_screen_sections/dashboard_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:onlinediagnostic_admin/blocs/dashboard_count/dashboard_count_bloc.dart';
 import 'package:onlinediagnostic_admin/ui/screens/home_screen_sections/order_management_section.dart';
 import 'package:onlinediagnostic_admin/ui/widgets/custom_action_button.dart';
 import 'package:onlinediagnostic_admin/ui/widgets/dashcard.dart';
+
+import '../../widgets/custom_alert_dialog.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -11,6 +15,14 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  final DashboardCountBloc dashboardCountBloc = DashboardCountBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    dashboardCountBloc.add(DashboardCountEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -20,31 +32,63 @@ class _DashboardScreenState extends State<DashboardScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(height: 40),
-            Wrap(
-              spacing: 20,
-              runSpacing: 20,
-              children: const [
-                DashCard(
-                  iconData: Icons.receipt_outlined,
-                  label: 'Total Orders Today',
-                  value: '340',
-                ),
-                DashCard(
-                  iconData: Icons.local_hospital_outlined,
-                  label: 'Total Nurses',
-                  value: '26',
-                ),
-                DashCard(
-                  iconData: Icons.text_snippet_outlined,
-                  label: 'Total Tests',
-                  value: '12',
-                ),
-                DashCard(
-                  iconData: Icons.sick_outlined,
-                  label: 'Total Patients',
-                  value: '16',
-                ),
-              ],
+            BlocProvider<DashboardCountBloc>.value(
+              value: dashboardCountBloc,
+              child: BlocConsumer<DashboardCountBloc, DashboardCountState>(
+                listener: (context, state) {
+                  if (state is DashboardCountFailureState) {
+                    showDialog(
+                      context: context,
+                      builder: (context) => CustomAlertDialog(
+                        title: 'Failure',
+                        message: state.message,
+                        primaryButtonLabel: 'Ok',
+                        primaryOnPressed: () {
+                          dashboardCountBloc.add(DashboardCountEvent());
+                          Navigator.pop(context);
+                        },
+                      ),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return state is DashboardCountSuccessState
+                      ? Wrap(
+                          spacing: 20,
+                          runSpacing: 20,
+                          children: [
+                            DashCard(
+                              iconData: Icons.receipt_outlined,
+                              label: 'Pending Bookings',
+                              value: state.dashbordCount['test_booking_count']
+                                  .toString(),
+                            ),
+                            DashCard(
+                              iconData: Icons.local_hospital_outlined,
+                              label: 'Total Nurses',
+                              value:
+                                  state.dashbordCount['nurse_count'].toString(),
+                            ),
+                            DashCard(
+                              iconData: Icons.text_snippet_outlined,
+                              label: 'Total Tests',
+                              value:
+                                  state.dashbordCount['test_count'].toString(),
+                            ),
+                            DashCard(
+                              iconData: Icons.sick_outlined,
+                              label: 'Total Patients',
+                              value: state.dashbordCount['patient_count']
+                                  .toString(),
+                            ),
+                          ],
+                        )
+                      : const Padding(
+                          padding: EdgeInsets.all(50.0),
+                          child: Center(child: CircularProgressIndicator()),
+                        );
+                },
+              ),
             ),
             const Expanded(
               child: OrderManagementSection(
